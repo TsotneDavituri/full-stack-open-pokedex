353e732369d68f9a20e4834034cc963b58702b2a
Tsotne Davituri <tsotne.davituri@gmail.com>
2024-01-03 15:28:14 +0100

    toml config test

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
+  console.log('sending ok response')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('9')
+  console.log('sending version response')
+  res.send('11')
 })
+
+/* app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+}) */
